a9d76ec01b372db13b5e782add22004d98edc9cb
Uthaeus <romanlavery@gmail.com>
2024-03-08 10:47:41 -0700

    added styling to edit, new, and form for orders

--- src/components/orders/edit-order.js
+++ src/components/orders/edit-order.js
@@ -1,4 +1,4 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import { useContext, useState, useEffect } from "react";
 
 import {OrdersContext} from "../../store/orders-context";
@@ -6,24 +6,37 @@ import OrderForm from "./order-form";
 
 function EditOrder() {
     const { id } = useParams();
-    const { orders } = useContext(OrdersContext);
+    const { orders, removeOrder } = useContext(OrdersContext);
+
+    const navigate = useNavigate();
 
     const [order, setOrder] = useState(null);
 
     const user = {
         id: 1,
         name: "John Doe",
+        role: 'admin'
     };
 
     useEffect(() => {
         setOrder(orders.find((order) => +order.id === +id));
     }, [orders, id]);
+    
+    const deleteOrderHandler = () => {
+        removeOrder(order.id);
+        navigate('/');
+    };
 
     return (
         <div className="order-container">
             <h1 className="order-title">Edit Order</h1>
+            
             {order && <OrderForm order={order} user={user} />}
-            <Link to='/' className="order-link">Back to Home</Link>
+
+            <div className="order-actions-wrapper">
+                {user?.role === 'admin' && <p className="order-action-button order-action-button-delete" onClick={deleteOrderHandler}>Delete Order</p>}
+                <Link to='/' className="order-action-button order-action-button-back">Back to Home</Link>
+            </div>
         </div>
     );
 }
